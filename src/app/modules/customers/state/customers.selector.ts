d39539a7d6e2cc9aabbf691bcf0fe82c344f4a29
Edycar Reyes <edycarr@nicasource.com>
2022-09-02 10:35:37 -0600

    creted the selector to find a customer by id

--- src/app/modules/customers/state/customers.selector.ts
+++ src/app/modules/customers/state/customers.selector.ts
@@ -1,4 +1,9 @@
-import {createFeatureSelector} from "@ngrx/store";
+import {createFeatureSelector, createSelector} from "@ngrx/store";
 import {ICustomer} from "../interfaces/customer";
 
 export const selectCustomers = createFeatureSelector<ICustomer[]>('customers');
+
+export const selectCustomerById = (customerId: string) => createSelector(
+  selectCustomers,
+  (customers: ICustomer[]) => customers.find((customer) => customer.id === customerId)
+);
