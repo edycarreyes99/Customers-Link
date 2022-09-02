56c81c19a21768c80e7181b62fce90a490d8e78f
Edycar Reyes <edycarr@nicasource.com>
2022-09-02 11:30:47 -0600

    created the logic for the no authenticated guard

--- src/app/core/guards/no-authenticated/no-authenticated.guard.ts
+++ src/app/core/guards/no-authenticated/no-authenticated.guard.ts
@@ -1,15 +1,27 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {USER_SIGNED_IN_LS} from "../../constants/local-storage.constants";
+import {NavigationService} from "../../services/navigation/navigation.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoAuthenticatedGuard implements CanActivate {
+  constructor(
+    private navigationService: NavigationService
+  ) {
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    if (localStorage.getItem(USER_SIGNED_IN_LS)) {
+      this.navigationService.navigate('customers');
+      return false;
+    } else {
+      return true;
+    }
   }
-  
+
 }
