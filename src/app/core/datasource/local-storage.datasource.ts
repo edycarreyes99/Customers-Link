7a2313a205342094ff875628898645e57ae7a108
Edycar Reyes <edycarr@nicasource.com>
2022-09-02 00:58:26 -0600

    generated all the CRUD logic for the local-storage datasource.

--- src/app/core/datasource/local-storage.datasource.ts
+++ src/app/core/datasource/local-storage.datasource.ts
@@ -1,6 +1,7 @@
 import {HttpClient} from '@angular/common/http';
 import {ICRUD} from "../interfaces/crud";
 import {CUSTOMERS_LS} from "../constants/local-storage.constants";
+import {faker} from "@faker-js/faker";
 
 export abstract class LocalStorageDatasource<M, S> implements ICRUD<M, S> {
 
@@ -9,27 +10,77 @@ export abstract class LocalStorageDatasource<M, S> implements ICRUD<M, S> {
   ) {
   }
 
-  index(): M {
-    return {} as M;
+  index(): S[] {
+    if (!this.verifyIfDataSourceExists()) {
+      this.poblate();
+    }
+
+    return JSON.parse(localStorage.getItem(CUSTOMERS_LS)!) as S[];
   }
 
-  store(body: object): S {
-    return {} as S;
+  store(body: S): S {
+    if (!this.verifyIfDataSourceExists()) {
+      this.poblate();
+    }
+
+    const data = this.index();
+
+    (body as any).id = faker.datatype.uuid();
+
+    (data as any).push(body);
+
+    this.poblate(data);
+
+    return body;
   }
 
-  show(id: number | string): S {
-    return {} as S;
+  show(id: number | string): S | undefined {
+    if (!this.verifyIfDataSourceExists()) {
+      this.poblate();
+    }
+
+    return this.index().find(d => (d as any).id === id);
   }
 
   update(id: number | string, body: S): S {
-    return {} as S;
+    if (!this.verifyIfDataSourceExists()) {
+      this.poblate();
+    }
+
+    const data = this.index();
+
+    const index = data.findIndex(d => (d as any).id === id);
+
+    data[index] = body;
+
+    this.poblate(data);
+
+    return body;
   }
 
   delete(id: number | string): S {
-    return {} as S;
+    if (!this.verifyIfDataSourceExists()) {
+      this.poblate();
+    }
+
+    const data = this.index();
+
+    const index = data.findIndex(d => (d as any).id === id);
+
+    const deleted = data[index];
+
+    data.splice(index, 1);
+
+    this.poblate(data);
+
+    return deleted;
   }
 
-  poblate(data: M[]): void {
-    localStorage.setItem(CUSTOMERS_LS, JSON.stringify(data));
+  poblate(data?: S[]): void {
+    localStorage.setItem(CUSTOMERS_LS, JSON.stringify(data ?? []));
+  }
+
+  verifyIfDataSourceExists(): boolean {
+    return !!localStorage.getItem(CUSTOMERS_LS);
   }
 }
--- src/app/core/interfaces/crud.ts
+++ src/app/core/interfaces/crud.ts
@@ -1,15 +1,15 @@
 import {Observable} from "rxjs";
 
 export interface ICRUD<M, S> {
-  index(): M;
+  index(): S[];
 
   store(params: any): S;
 
-  show?(id: number | string): S;
+  show?(id: number | string): S | undefined;
 
   update(id: number | string, t: S): S;
 
   delete(id: number | string): S;
 
-  poblate(data: M[]): void;
+  poblate(data: S[]): void;
 }
