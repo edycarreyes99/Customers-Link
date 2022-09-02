812a6dbffec20aaa2a46e9bff010cd557e14305b
Edycar Reyes <edycarr@nicasource.com>
2022-09-02 10:31:41 -0600

    creted the reducer to edit a customer

--- src/app/modules/customers/state/customers.reducer.ts
+++ src/app/modules/customers/state/customers.reducer.ts
@@ -1,11 +1,16 @@
 import {ICustomer} from "../interfaces/customer";
 import {createReducer, on} from "@ngrx/store";
-import {customersFetchDataSourceSuccess, saveNewCustomerSuccess} from "./customers.action";
+import {customersFetchDataSourceSuccess, editCustomerSuccess, saveNewCustomerSuccess} from "./customers.action";
 
 export const initialState: ReadonlyArray<ICustomer> = [];
 
 export const customersReducer = createReducer(
   initialState,
   on(customersFetchDataSourceSuccess, (state, {customers}) => customers),
-  on(saveNewCustomerSuccess, (state, {customer}) => [...state, customer])
+  on(saveNewCustomerSuccess, (state, {customer}) => [...state, customer]),
+  on(editCustomerSuccess, (state, {customer}) => {
+    let newState = state.filter((_) => _.id !== customer.id);
+    newState.unshift(customer);
+    return newState;
+  })
 );
