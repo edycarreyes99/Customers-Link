c93f7dfa53b05ad443b46ba82a154aecd82baf4a
Edycar Reyes <edycarr@nicasource.com>
2022-09-01 23:26:02 -0600

    added the method 'navigate' to navigate between routes in the project to the navigation-service file

--- src/app/core/services/navigation/navigation.service.ts
+++ src/app/core/services/navigation/navigation.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavigationService {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) {
+  }
+
+  // Method to navigate to other routes
+  async navigate(ruta: string, params?: any, relativeTo?: ActivatedRoute): Promise<boolean> {
+    return this.router.navigate([`/${ruta}`], {
+      queryParams: params ? params : {},
+      relativeTo
+    });
+  }
 }
