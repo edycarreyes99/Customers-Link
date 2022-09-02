a3de3d7c6d43a93ccef76d31a1d2e1154329a9f5
Edycar Reyes <edycarr@nicasource.com>
2022-09-02 11:36:51 -0600

    implemented the logic to logout from the app

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {USER_SIGNED_IN_LS} from "../../constants/local-storage.constants";
+import {NavigationService} from "../../services/navigation/navigation.service";
+import {ToastService} from "../../services/toast/toast.service";
+import {SUCCESS_TOAST} from "../../constants/toast.constants";
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private navigationService: NavigationService,
+    private toastSetvice: ToastService
+  ) {
+  }
 
   ngOnInit(): void {
+  }
+
+  logout(): void {
+    const currentUser = localStorage.getItem(USER_SIGNED_IN_LS) ?? '';
+    localStorage.removeItem(USER_SIGNED_IN_LS);
+    this.navigationService.navigate('login');
+    this.toastSetvice.showToast(SUCCESS_TOAST, 'Logout', `${currentUser} logged out. Welcome back!`);
   }
 
 }
