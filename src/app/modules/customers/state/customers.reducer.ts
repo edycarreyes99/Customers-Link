55594b68cfa8f19d8ad4377b04ed66c01e39e28b
Edycar Reyes <edycarr@nicasource.com>
2022-09-02 09:07:18 -0600

    created the reducer to save a new customer

--- src/app/modules/customers/state/customers.reducer.ts
+++ src/app/modules/customers/state/customers.reducer.ts
@@ -1,10 +1,11 @@
 import {ICustomer} from "../interfaces/customer";
 import {createReducer, on} from "@ngrx/store";
-import {customersFetchDataSourceSuccess} from "./customers.action";
+import {customersFetchDataSourceSuccess, saveNewCustomerSuccess} from "./customers.action";
 
 export const initialState: ReadonlyArray<ICustomer> = [];
 
 export const customersReducer = createReducer(
   initialState,
-  on(customersFetchDataSourceSuccess, (state, {customers}) => customers)
+  on(customersFetchDataSourceSuccess, (state, {customers}) => customers),
+  on(saveNewCustomerSuccess, (state, {customer}) => [...state, customer])
 );
