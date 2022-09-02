9cde8eec7c398976e9c7823f0f2da2c504f0ef01
Edycar Reyes <edycarr@nicasource.com>
2022-09-01 19:47:18 -0600

    creted the customers-view component in the customer's module

--- src/app/modules/customers/customers.module.ts
+++ src/app/modules/customers/customers.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CustomersRoutingModule } from './customers-routing.module';
+import { CustomersViewComponent } from './components/customers-view/customers-view.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CustomersViewComponent
+  ],
   imports: [
     CommonModule,
     CustomersRoutingModule
