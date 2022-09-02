fe0c7481d01e72ebbfd153ca1611adbb0f735a55
Edycar Reyes <edycarr@nicasource.com>
2022-09-01 19:49:25 -0600

    added the customers routes to the customers-routing.module file

--- src/app/modules/customers/customers-routing.module.ts
+++ src/app/modules/customers/customers-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CustomersViewComponent} from "./components/customers-view/customers-view.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CustomersViewComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class CustomersRoutingModule { }
+export class CustomersRoutingModule {
+}
