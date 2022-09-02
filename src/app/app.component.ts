c0f050cb96e2d49e8f8d1bf6dfd992e5d0bd6dcc
Edycar Reyes <edycarr@nicasource.com>
2022-09-02 11:22:20 -0600

    implemented the logic for hide the navbar based on the navigation url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  // Component Variables
   title = 'customers-link';
+  hideNavbar = true;
+
+  constructor(
+    private router: Router
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        switch (event.url) {
+          case '/login' : {
+            console.log('Hide navbar?: true')
+            this.hideNavbar = true;
+            break;
+          }
+          default : {
+            console.log('Hide navbar?: false')
+            this.hideNavbar = false;
+          }
+        }
+      }
+    });
+  }
 }
