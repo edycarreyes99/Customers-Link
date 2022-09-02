fb454e682eed4c5407ef188dbdab23fdb0129a8d
Edycar Reyes <edycarr@nicasource.com>
2022-09-02 08:58:19 -0600

    created the save new customer actions

--- src/app/modules/customers/state/customers.action.ts
+++ src/app/modules/customers/state/customers.action.ts
@@ -2,5 +2,7 @@ import {createAction, props} from "@ngrx/store";
 import {ICustomer} from "../interfaces/customer";
 
 export const invokeCustomersDataSource = createAction('[Customers] Invoke Customers Fetch Data from Data Source');
+export const invokeSaveNewCustomers = createAction('[Customers] Invoke Save new Customer', props<{ customer: ICustomer }>());
 
 export const customersFetchDataSourceSuccess = createAction('[Customers] Customers Fetched from Data Source Successfully', props<{ customers: ICustomer[] }>());
+export const saveNewCustomerSuccess = createAction('[Customers] Customer Saved to Data Source Successfully', props<{ customer: ICustomer }>());
