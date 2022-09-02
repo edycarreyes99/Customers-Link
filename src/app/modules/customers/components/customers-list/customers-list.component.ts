c305653fe315917fa62a47468dd31591289034ea
Edycar Reyes <edycarr@nicasource.com>
2022-09-02 00:08:34 -0600

    designed the customers list

--- src/app/modules/customers/components/customers-list/customers-list.component.ts
+++ src/app/modules/customers/components/customers-list/customers-list.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {MatSort} from "@angular/material/sort";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatTableDataSource} from "@angular/material/table";
+import {ICustomer} from "../../interfaces/customer";
 
 @Component({
   selector: 'app-customers-list',
   templateUrl: './customers-list.component.html',
   styleUrls: ['./customers-list.component.scss']
 })
-export class CustomersListComponent implements OnInit {
+export class CustomersListComponent implements OnInit, AfterViewInit {
+  // ViewChild Variables
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+  @ViewChild(MatSort) sort: MatSort | undefined;
 
-  constructor() { }
+  // Component Variables
+  customers: ICustomer[] = [];
+  displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
+  dataSource: MatTableDataSource<ICustomer>;
+
+  constructor() {
+    this.dataSource = new MatTableDataSource(this.customers);
+  }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator ?? null;
+    this.dataSource.sort = this.sort ?? null;
+  }
+
+  searchCustomer(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
--- src/app/modules/customers/customers.module.ts
+++ src/app/modules/customers/customers.module.ts
@@ -4,6 +4,11 @@ import { CommonModule } from '@angular/common';
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomersViewComponent } from './components/customers-view/customers-view.component';
 import { CustomersListComponent } from './components/customers-list/customers-list.component';
+import {MatTableModule} from "@angular/material/table";
+import {MatSortModule} from "@angular/material/sort";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
 
 
 @NgModule({
@@ -13,7 +18,12 @@ import { CustomersListComponent } from './components/customers-list/customers-li
   ],
   imports: [
     CommonModule,
-    CustomersRoutingModule
+    CustomersRoutingModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class CustomersModule { }
